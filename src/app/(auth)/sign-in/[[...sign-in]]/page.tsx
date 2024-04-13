a2a3013240ce03e1615f4f048414e8306daf0b9e
Rakshith <rakkshith2001@gmail.com>
2024-04-13 17:36:52 +0530

    clerk commit

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,7 @@
-import { SignIn } from "@clerk/nextjs";
- 
-export default function Page() {
-  return <SignIn />;
+import { SignIn } from '@clerk/nextjs'
+
+const SignInPage = () => {
+  return <SignIn />
 }
+
+export default SignInPage
